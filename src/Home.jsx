930523dc7e950f430bbb68e0e348efeaea6eaa48
Anderson-77-xd <rm95071@estudante.fieb.edu.br>
2025-06-03 10:38:33 +0000

    flex

--- src/Home.jsx
+++ src/Home.jsx
@@ -13,6 +13,7 @@ const Home = () => {
             <img src={poke} />
             <img src={eevee} />
             <img src={poke} />
+            <img src={eevee} />
             <img src={poke} />
             <img src={eevee} />
         </div>
