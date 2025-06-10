c8e65bf3be23e654d3bbe7c9743ccf4582060176
Anderson-77-xd <rm95071@estudante.fieb.edu.br>
2025-06-10 10:44:35 +0000

    menu

---
+++ src/Menu.jsx
@@ -0,0 +1,23 @@
+const Menu = () => {
+
+    return (
+        <div className="Menu">
+        <nav>
+         <ul>
+         <li>
+        <a href="/Home">Home</a>
+        </li>
+        <li>
+        <a href="/pokefelx">Flex</a>
+        </li>
+        <li>
+        <a href="/pokegrid">Grid</a>
+
+         </li>
+         </ul>
+        </nav>
+    </div>
+ )
+}
+
+export default Menu
